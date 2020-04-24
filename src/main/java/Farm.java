5526f26212457cd20068f6584f8ad9e0665bf362
Enci21 <eni.hegedus.95@gmail.com>
2020-04-24 15:36:41 +0200

    create farm bulider

--- src/main/java/Farm.java
+++ src/main/java/Farm.java
@@ -1,17 +1,46 @@
 import plant.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class Farm {
 
-    private List<Plant> plants = new ArrayList<Plant>();
-    private Month month;
+    private List<Plant> plants = new ArrayList();
+    private Month month = new Month();
     private Random random = new Random();
 
-    public Farm(Month month) {
+    private Farm(Month month, List<Plant> plants) {
         this.month = month;
+    }
+
+    public static class FarmBuilder {
+
+        private List<Plant> plants = new ArrayList();
+        private Month month = new Month();
+
+        public FarmBuilder addSpruc(Spruc spruc) {
+            plants.add(spruc);
+            return this;
+        }
+
+        public FarmBuilder addJuniper(Juniper juniper) {
+            plants.add(juniper);
+            return this;
+        }
+
+        public FarmBuilder addAlmond(Almond almond) {
+            plants.add(almond);
+            return this;
+        }
+
+        public Farm build() throws RuntimeException {
+            if (Collections.frequency(plants, Almond.class) >= 2 && Collections.frequency(plants, Spruc.class) >= 2 && Collections.frequency(plants, Juniper.class) >= 2) {
+                return new Farm(month, plants);
+            }
+            throw new RuntimeException("At least 2 of every plant type needed!");
+        }
     }
 
     public void createPlants() {
@@ -22,8 +51,7 @@ public class Farm {
         if (random.nextInt(100) < 66) {
             Juniper juniper = new Juniper(month);
             plants.add(juniper);
-        }
-        else {
+        } else {
             Spruc spruc = new Spruc(month);
             plants.add(spruc);
         }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,26 @@
+import plant.Almond;
+import plant.Juniper;
 import plant.Month;
+import plant.Spruc;
 
 public class Main {
 
 
     public static void main(String[] args) {
         Month month = new Month();
-        Farm farm = new Farm(month);
+        Juniper juniper = new Juniper(month);
+        Juniper juniper2 = new Juniper(month);
+        Spruc spruc = new Spruc(month);
+        Spruc spruc2 = new Spruc(month);
+        Spruc spruc3 = new Spruc(month);
+        Spruc spruc4 = new Spruc(month);
+        Almond almond = new Almond();
+        Almond almond2 = new Almond();
+        Farm farm = new Farm.FarmBuilder().addAlmond(almond).addAlmond(almond2).addJuniper(juniper).addJuniper(juniper2).addSpruc(spruc).addSpruc(spruc2).build();
         farm.createPlants();
-        for (int i=0 ; i < 80; i++){
-            month.setMonthCounter(month.getMonthCounter()+1);
-            System.out.println("TOTAL FOOD PRODUCTION on the Farm after " + month.getMonthCounter() +" months: " + farm.countTotalFood());
+        for (int i = 0; i < 80; i++) {
+            month.setMonthCounter(month.getMonthCounter() + 1);
+            System.out.println("TOTAL FOOD PRODUCTION on the Farm after " + month.getMonthCounter() + " months: " + farm.countTotalFood());
         }
 
     }
